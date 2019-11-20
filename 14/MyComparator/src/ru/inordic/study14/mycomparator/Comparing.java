68c1fe88de976d85985c5b8ee087f7fdeafe3c15
Sergey Zhukov <zhukovsv@gmail.com>
2019-11-20 20:20:53 +0300

    Comparator added

---
+++ 14/MyComparator/src/ru/inordic/study14/mycomparator/Comparing.java
@@ -0,0 +1,35 @@
+package ru.inordic.study14.mycomparator;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import ru.inordic.study14.mycomparator.*;
+
+public class Comparing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		var scanner = new Scanner(System.in);
+		var numbers = new ArrayList<String>();
+		
+		System.out.println("Введите числа: ");
+		
+		var inputString = scanner.nextLine();
+		
+		while (!"".equals(inputString)) {
+			numbers.add(inputString);
+			inputString = scanner.nextLine();
+		}
+		
+		System.out.print("Перед сортировкой - ");
+		System.out.println(numbers);
+		
+		numbers.sort(new MyComparator());
+		
+		System.out.print("После сортировки - ");
+		System.out.println(numbers);
+	
+	}
+
+}
---
+++ 14/MyComparator/src/ru/inordic/study14/mycomparator/MyComparator.java
@@ -0,0 +1,13 @@
+package ru.inordic.study14.mycomparator;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<String> {
+
+	@Override
+	public int compare(String a, String b) {
+		// TODO Auto-generated method stub	
+		return Integer.parseInt(a) < Integer.parseInt(b) ? -1 : 1;
+	}
+
+}
