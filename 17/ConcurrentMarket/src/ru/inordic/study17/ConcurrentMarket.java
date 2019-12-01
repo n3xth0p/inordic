82e452e7d0b94f32225a3a75f434ef27fc21e1c1
Sregey Zhukov <zhukovsv@gmail.com>
2019-12-01 03:53:21 +0300

    Market with Concurrent lib

---
+++ 17/ConcurrentMarket/src/ru/inordic/study17/ConcurrentMarket.java
@@ -0,0 +1,51 @@
+package ru.inordic.study17;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class ConcurrentMarket {
+
+  private static LinkedBlockingDeque<Integer> clients = new LinkedBlockingDeque<Integer>();
+  private static LinkedBlockingDeque<Integer> result = new LinkedBlockingDeque<Integer>();
+
+  public static void main(String[] args) throws InterruptedException {
+
+
+    for (int i = 1; i <= 2000; i++) {
+      clients.add(i);
+    }
+
+    System.out.println(clients);
+    System.out.println("size - " + clients.size());
+
+    var executorService = Executors.newFixedThreadPool(4);
+
+    for (int i = 0; i < 4; i++) {
+
+      executorService.execute(new Runnable() {
+
+        @Override
+        public void run() {
+          // TODO Auto-generated method stub
+          Integer client;
+
+          do {
+            client = clients.pollFirst();
+            if (client == null) {
+              break;
+            } else {
+              result.add(client);
+            }
+
+          } while (client != null);
+        }
+
+      });
+    }
+
+    System.out.println(result);
+    System.out.println("size - " + result.size());
+
+  }
+
+}
