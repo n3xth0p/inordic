9039ba7bf75c7edaa14693da35309336b1fe1acf
Sergey Zhukov <zhukovsv@gmail.com>
2020-01-12 00:01:47 +0300

    update with refactor and fix error
    Signed-off-by: Sergey Zhukov <zhukovsv@gmail.com>

--- 15/ThreadApplication/src/ru/inordic/study15/MyThreadApplication.java
+++ 15/ThreadApplication/src/ru/inordic/study15/MyThreadApplication.java
@@ -1,50 +1,57 @@
 package ru.inordic.study15;
-import ru.inordic.study15.ProductionStage;
-import java.util.ArrayList;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyThreadApplication {
 
-	static AtomicInteger stageCounter = new AtomicInteger();
-	
-	public static void main(String[] args) throws InterruptedException {
+  // static AtomicInteger stageCounter = new AtomicInteger();
 
-		
-		for (int j = 1; j <= 20; j++) {
-		
-			System.out.println("!!!!! Процесс производства по детали номер №" + j + " начат !!!!!");
+  public static void main(String[] args) throws InterruptedException {
 
-			var Thread1 = new Thread(new ProductionStage(stageCounter));
-			var Thread2 = new Thread(new ProductionStage(stageCounter));
+    var stageCounter = new AtomicInteger();
+    stageCounter.set(1);
 
-			Thread1.start();
-			Thread2.start();
-			
-			Thread1.join();
-			Thread2.join();
+    for (int j = 1; j <= 20; j++) {
 
-			var Thread3 = new Thread(new ProductionStage(stageCounter));
-			Thread3.start();
-			Thread3.join();
+      System.out.println("!!!!! Процесс производства по детали номер №" + j + " начат !!!!!");
 
-			
-			var Thread4 = new Thread(new ProductionStage(stageCounter));
-			var Thread5 = new Thread(new ProductionStage(stageCounter));
+      var thread1 = new Thread(new ProductionStage(stageCounter));
+      var thread2 = new Thread(new ProductionStage(stageCounter));
 
-			Thread4.start();
-			Thread5.start();
-		
-			Thread4.join();
-			Thread5.join();
-			
-			
-			var Thread6 = new Thread(new ProductionStage(stageCounter));
-			Thread6.start();
-			Thread6.join();
-			
-			System.out.println("===== Завершен процесс производства детали номер №" + j + " ====");
-			stageCounter.set(0);
-			
-		}
-	}
-}                                                        
+      thread1.start();
+      thread2.start();
+
+      thread1.join();
+      thread2.join();
+
+      var thread3 = new Thread(new ProductionStage(stageCounter));
+      thread3.start();
+      thread3.join();
+
+
+      var thread4 = new Thread(new ProductionStage(stageCounter));
+      var thread5 = new Thread(new ProductionStage(stageCounter));
+
+      thread4.start();
+      thread5.start();
+
+      thread4.join();
+      thread5.join();
+
+
+      System.out.println("===== Завершен процесс производства детали номер №" + j + " ====");
+      stageCounter.set(1);
+    }
+
+
+    var thread6 = new Thread(new ProductionStage(stageCounter));
+
+    System.out.println("===== Запущен финальный процесс сборки ====");
+
+    stageCounter.set(6);
+    thread6.start();
+    thread6.join();
+
+    System.out.println("===== Завершен финальный процесс сборки ====");
+  }
+}
--- 15/ThreadApplication/src/ru/inordic/study15/ProductionStage.java
+++ 15/ThreadApplication/src/ru/inordic/study15/ProductionStage.java
@@ -1,26 +1,31 @@
 package ru.inordic.study15;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class ProductionStage implements Runnable {
-	
-	private static AtomicInteger stageNum = new AtomicInteger();
 
-	public ProductionStage(AtomicInteger stageNum) {
-		super();
-		this.stageNum = stageNum;
-	}
+  private static AtomicInteger stageNum;
 
-	
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		stageNum.getAndIncrement();
-		System.out.println("процесс обработки №" + stageNum);
-		
-	}
-	
-	
-	
+  public ProductionStage(AtomicInteger stageNum) {
+    super();
+    this.stageNum = stageNum;
+  }
+
+
+  @Override
+  public void run() {
+    // TODO Auto-generated method stub
+    // stageNum.getAndIncrement();
+    // System.out.println("процесс обработки №" + stageNum);
+    if (stageNum.get() == 6) {
+      System.out.println("процесс сборки выполняется");
+    } else {
+      var number = stageNum.getAndIncrement();
+      System.out.println("процесс обработки №" + number);
+    }
+  }
+
+
 
 }
